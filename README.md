b64d61a81cf7149b54bd633c460c215e81ef9702
NawfalLakouidi <nawfallakouidi68@gmail.com>
2020-10-29 18:42:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-1u9s54
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-1u9s54)
---
+++ index.ts
@@ -0,0 +1,5 @@
+let Mynum1, Mynum2, Result:number;
+
+Mynum1= parseInt(prompt("écrit le premier number"));
+Mynum2= parseInt(prompt("écrit le deuxieme number"));
+
